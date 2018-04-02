c661522b27ec1552c829036f2a5d596170339f82
Max Burnette <mburnet2@illinois.com>
2018-04-02 09:04:38 -0500

    add description to pypi listing

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import find_packages, setup
 
+def description():
+      with open('readme.rst') as f:
+            return f.read()
+
 setup(name='terraref-stereo_rgb',
-      version='1.0.3',
+      version='1.0.4',
+      description='TERRA-REF stereo RGB camera science package',
+      long_description=description(),
       packages=find_packages(),
       namespace_packages=['terraref'],
       include_package_data=True,
--- terraref/stereo_rgb/__init__.py
+++ terraref/stereo_rgb/__init__.py
@@ -2,3 +2,4 @@ __author__ = 'weiqin'
 
 from .stereo_rgb import \
     calculate_canopycover,  process_raw,  demosaic, get_image_shape
+
